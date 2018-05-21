842bf0379c5980931aca10673fa8b80a213b7543
vigneshwarr1990 <vigneshwarr1990@gmail.com>
2018-05-20 21:26:23 -0500

    Initial commit

---
+++ src/main/java/ReactorSnippets.java
@@ -0,0 +1,45 @@
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReactorSnippets {
+
+    private static List<String> words = Arrays.asList(
+            "the",
+            "quick",
+            "brown",
+            "fox",
+            "jumped",
+            "over",
+            "the",
+            "lazy",
+            "dog"
+    );
+
+    public static void main(String[] args) {
+        simpleCreation();
+        findingMissingLetter();
+    }
+
+    public static void simpleCreation() {
+        Flux<String> fewWords = Flux.just("Hello", "World");
+        Flux<String> manyWords = Flux.fromIterable(words);
+
+        fewWords.subscribe(System.out::println);
+        System.out.println();
+        manyWords.subscribe(System.out::println);
+    }
+
+    public static void findingMissingLetter() {
+        Flux<String> manyLetters = Flux
+                .fromIterable(words)
+                .flatMap(word -> Flux.fromArray(word.split("")))
+                .distinct()
+                .sort()
+                .zipWith(Flux.range(1, Integer.MAX_VALUE),
+                        (string, count) -> String.format("%2d. %s", count, string));
+
+        manyLetters.subscribe(System.out::println);
+    }
+}
